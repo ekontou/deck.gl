c5abdfb2c8e55ab5632b0554dd1251ca0e123acb
Xiaoji Chen <xiaoji@uber.com>
2017-12-10 14:06:44 -0800

    add performant SolidPolygonLayer

---
+++ src/experimental-layers/src/solid-polygon-layer/polygon-tesselator.js
@@ -0,0 +1,340 @@
+// Copyright (c) 2015 - 2017 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+// Handles tesselation of polygons with holes
+// - 2D surfaces
+// - 2D outlines
+// - 3D surfaces (top and sides only)
+// - 3D wireframes (not yet)
+import * as Polygon from './polygon';
+import earcut from 'earcut';
+import {experimental} from '../../core';
+const {get, count, flattenVertices, fillArray} = experimental;
+
+// Maybe deck.gl or luma.gl needs to export this
+function getPickingColor(index) {
+  return [
+    (index + 1) % 256,
+    Math.floor((index + 1) / 256) % 256,
+    Math.floor((index + 1) / 256 / 256) % 256
+  ];
+}
+
+function parseColor(color) {
+  if (!Array.isArray(color)) {
+    color = [get(color, 0), get(color, 1), get(color, 2), get(color, 3)];
+  }
+  color[3] = Number.isFinite(color[3]) ? color[3] : 255;
+  return color;
+}
+
+const DEFAULT_COLOR = [0, 0, 0, 255]; // Black
+
+// This class is set up to allow querying one attribute at a time
+// the way the AttributeManager expects it
+export class PolygonTesselator {
+  constructor(polygons) {
+    // Normalize all polygons
+    polygons = polygons.map(polygon => Polygon.normalize(polygon));
+
+    // Count all polygon vertices
+    const pointCount = getPointCount(polygons);
+
+    this.polygons = polygons;
+    this.pointCount = pointCount;
+
+    this.attributes = {
+      pickingColors: calculatePickingColors({polygons, pointCount})
+    };
+  }
+
+  updatePositions({fp64, extruded}) {
+    const {attributes, polygons, pointCount} = this;
+
+    attributes.positions = attributes.positions || new Float32Array(pointCount * 3);
+    attributes.nextPositions = attributes.nextPositions || new Float32Array(pointCount * 3);
+
+    if (fp64) {
+      // We only need x, y component
+      attributes.positions64xyLow = attributes.positions64xyLow ||
+        new Float32Array(pointCount * 2);
+      attributes.nextPositions64xyLow = attributes.nextPositions64xyLow ||
+        new Float32Array(pointCount * 2);
+    }
+
+    updatePositions(attributes, {polygons, extruded, fp64});
+  }
+
+  indices() {
+    const {polygons, indexCount} = this;
+    return calculateIndices({polygons, indexCount});
+  }
+
+  positions() {
+    return this.attributes.positions;
+  }
+  positions64xyLow() {
+    return this.attributes.positions64xyLow;
+  }
+
+  nextPositions() {
+    return this.attributes.nextPositions;
+  }
+  nextPositions64xyLow() {
+    return this.attributes.nextPositions64xyLow;
+  }
+
+  elevations({getElevation = x => 100} = {}) {
+    const {attributes, polygons, pointCount} = this;
+    attributes.elevations = attributes.elevations || new Float32Array(pointCount);
+    return updateElevations(attributes, {polygons, getElevation});
+  }
+
+  colors({getColor = x => DEFAULT_COLOR} = {}) {
+    const {attributes, polygons, pointCount} = this;
+    attributes.colors = attributes.colors || new Uint8ClampedArray(pointCount * 4);
+    return updateColors(attributes, {polygons, getColor});
+  }
+
+  pickingColors() {
+    return this.attributes.pickingColors;
+  }
+
+  // getAttribute({size, accessor}) {
+  //   const {polygons, pointCount} = this;
+  //   return calculateAttribute({polygons, pointCount, size, accessor});
+  // }
+}
+
+// Count number of points in a list of complex polygons
+function getPointCount(polygons) {
+  return polygons.reduce((points, polygon) => points + Polygon.getVertexCount(polygon), 0);
+}
+
+// COunt number of triangles in a list of complex polygons
+function getTriangleCount(polygons) {
+  return polygons.reduce((triangles, polygon) => triangles + Polygon.getTriangleCount(polygon), 0);
+}
+
+// Returns the offsets of each complex polygon in the combined array of all polygons
+function getPolygonOffsets(polygons) {
+  const offsets = new Array(count(polygons) + 1);
+  offsets[0] = 0;
+  let offset = 0;
+  polygons.forEach((polygon, i) => {
+    offset += Polygon.getVertexCount(polygon);
+    offsets[i + 1] = offset;
+  });
+  return offsets;
+}
+
+// Returns the offset of each hole polygon in the flattened array for that polygon
+function getHoleIndices(complexPolygon) {
+  let holeIndices = null;
+  if (count(complexPolygon) > 1) {
+    let polygonStartIndex = 0;
+    holeIndices = [];
+    complexPolygon.forEach(polygon => {
+      polygonStartIndex += count(polygon);
+      holeIndices.push(polygonStartIndex);
+    });
+    // Last element points to end of the flat array, remove it
+    holeIndices.pop();
+  }
+  return holeIndices;
+}
+
+function calculateIndices({polygons, IndexType = Uint32Array}) {
+  // Calculate length of index array (3 * number of triangles)
+  const indexCount = 3 * getTriangleCount(polygons);
+  const offsets = getPolygonOffsets(polygons);
+
+  // Allocate the attribute
+  // TODO it's not the index count but the vertex count that must be checked
+  if (IndexType === Uint16Array && indexCount > 65535) {
+    throw new Error('Vertex count exceeds browser\'s limit');
+  }
+  const attribute = new IndexType(indexCount);
+
+  // 1. get triangulated indices for the internal areas
+  // 2. offset them by the number of indices in previous polygons
+  let i = 0;
+  polygons.forEach((polygon, polygonIndex) => {
+    for (const index of calculateSurfaceIndices(polygon)) {
+      attribute[i++] = index + offsets[polygonIndex];
+    }
+  });
+
+  return attribute;
+}
+
+/*
+ * Get vertex indices for drawing complexPolygon mesh
+ * @private
+ * @param {[Number,Number,Number][][]} complexPolygon
+ * @returns {[Number]} indices
+ */
+function calculateSurfaceIndices(complexPolygon) {
+  // Prepare an array of hole indices as expected by earcut
+  const holeIndices = getHoleIndices(complexPolygon);
+  // Flatten the polygon as expected by earcut
+  const verts = flattenVertices2(complexPolygon);
+  // Let earcut triangulate the polygon
+  return earcut(verts, holeIndices, 3);
+}
+
+// TODO - refactor
+function isContainer(value) {
+  return Array.isArray(value) || ArrayBuffer.isView(value) ||
+    value !== null && typeof value === 'object';
+}
+
+// TODO - refactor, this file should not need a separate flatten func
+// Flattens nested array of vertices, padding third coordinate as needed
+export function flattenVertices2(nestedArray, {result = [], dimensions = 3} = {}) {
+  let index = -1;
+  let vertexLength = 0;
+  const length = count(nestedArray);
+  while (++index < length) {
+    const value = get(nestedArray, index);
+    if (isContainer(value)) {
+      flattenVertices(value, {result, dimensions});
+    } else {
+      if (vertexLength < dimensions) { // eslint-disable-line
+        result.push(value);
+        vertexLength++;
+      }
+    }
+  }
+  // Add a third coordinate if needed
+  if (vertexLength > 0 && vertexLength < dimensions) {
+    result.push(0);
+  }
+  return result;
+}
+
+function updatePositions(
+  {positions, positions64xyLow, nextPositions, nextPositions64xyLow},
+  {polygons, extruded, fp64}
+) {
+  // Flatten out all the vertices of all the sub subPolygons
+  let i = 0;
+  let j = 0;
+  let nextI = 0;
+  let nextJ = 0;
+  let startVertex = null;
+
+  const popStartVertex = () => {
+    if (startVertex && extruded) {
+      nextPositions[nextI++] = startVertex.x;
+      nextPositions[nextI++] = startVertex.y;
+      nextPositions[nextI++] = startVertex.z;
+      if (fp64) {
+        nextPositions64xyLow[nextJ++] = startVertex.xLow;
+        nextPositions64xyLow[nextJ++] = startVertex.yLow;
+      }
+    }
+    startVertex = null;
+  };
+
+  polygons.forEach((polygon, polygonIndex) => {
+    forEachVertex(polygon, (vertex, vertexIndex) => { // eslint-disable-line
+      const x = get(vertex, 0);
+      const y = get(vertex, 1);
+      const z = get(vertex, 2) || 0;
+      let xLow;
+      let yLow;
+
+      positions[i++] = x;
+      positions[i++] = y;
+      positions[i++] = z;
+      if (fp64) {
+        xLow = x - Math.fround(x);
+        yLow = y - Math.fround(y);
+        positions64xyLow[j++] = xLow;
+        positions64xyLow[j++] = yLow;
+      }
+      if (extruded && vertexIndex > 0) {
+        nextPositions[nextI++] = x;
+        nextPositions[nextI++] = y;
+        nextPositions[nextI++] = z;
+        if (fp64) {
+          nextPositions64xyLow[nextJ++] = xLow;
+          nextPositions64xyLow[nextJ++] = yLow;
+        }
+      }
+      if (vertexIndex === 0) {
+        popStartVertex();
+        startVertex = {x, y, z, xLow, yLow};
+      }
+    });
+  });
+  popStartVertex();
+}
+
+function updateElevations({elevations}, {polygons, getElevation}) {
+  let i = 0;
+  polygons.forEach((complexPolygon, polygonIndex) => {
+    // Calculate polygon color
+    const height = getElevation(polygonIndex);
+
+    const vertexCount = Polygon.getVertexCount(complexPolygon);
+    fillArray({target: elevations, source: [height], start: i, count: vertexCount});
+    i += vertexCount;
+  });
+  return elevations;
+}
+
+function updateColors({colors}, {polygons, getColor}) {
+  let i = 0;
+  polygons.forEach((complexPolygon, polygonIndex) => {
+    // Calculate polygon color
+    let color = getColor(polygonIndex);
+    color = parseColor(color);
+
+    const vertexCount = Polygon.getVertexCount(complexPolygon);
+    fillArray({target: colors, source: color, start: i, count: vertexCount});
+    i += color.length * vertexCount;
+  });
+  return colors;
+}
+
+function calculatePickingColors({polygons, pointCount}) {
+  const attribute = new Uint8ClampedArray(pointCount * 3);
+  let i = 0;
+  polygons.forEach((complexPolygon, polygonIndex) => {
+    const color = getPickingColor(polygonIndex);
+    const vertexCount = Polygon.getVertexCount(complexPolygon);
+    fillArray({target: attribute, source: color, start: i, count: vertexCount});
+    i += color.length * vertexCount;
+  });
+  return attribute;
+}
+
+function forEachVertex(polygon, visitor) {
+  if (Polygon.isSimple(polygon)) {
+    polygon.forEach(visitor);
+    return;
+  }
+
+  polygon.forEach(simplePolygon => {
+    simplePolygon.forEach(visitor);
+  });
+}
---
+++ src/experimental-layers/src/solid-polygon-layer/solid-polygon-layer.js
@@ -0,0 +1,346 @@
+// Copyright (c) 2015 - 2017 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import {COORDINATE_SYSTEM, Layer, experimental} from '../../core';
+const {enable64bitSupport, get} = experimental;
+import {GL, Model, Geometry} from 'luma.gl';
+
+// Polygon geometry generation is managed by the polygon tesselator
+import {PolygonTesselator} from './polygon-tesselator';
+
+import vs from './solid-polygon-layer-vertex.glsl';
+import vs64 from './solid-polygon-layer-vertex-64.glsl';
+import fs from './solid-polygon-layer-fragment.glsl';
+
+const defaultProps = {
+  // Whether to extrude
+  extruded: false,
+  // Whether to draw a GL.LINES wireframe of the polygon
+  wireframe: false,
+  fp64: false,
+
+  // elevation multiplier
+  elevationScale: 1,
+
+  // Accessor for polygon geometry
+  getPolygon: f => get(f, 'polygon') || get(f, 'geometry.coordinates'),
+  // Accessor for extrusion height
+  getElevation: f => get(f, 'elevation') || get(f, 'properties.height') || 0,
+  // Accessor for color
+  getColor: f => get(f, 'color') || get(f, 'properties.color'),
+
+  // Optional settings for 'lighting' shader module
+  lightSettings: {
+    lightsPosition: [-122.45, 37.75, 8000, -122.0, 38.00, 5000],
+    ambientRatio: 0.05,
+    diffuseRatio: 0.6,
+    specularRatio: 0.8,
+    lightsStrength: [2.0, 0.0, 0.0, 0.0],
+    numberOfLights: 2
+  }
+};
+
+// Side model attributes
+const SIDE_FILL_POSITIONS = new Float32Array([
+  // top left corner
+  0, 1,
+  // bottom left corner
+  0, 0,
+  // top right corner
+  1, 1,
+  // bottom right corner
+  1, 0
+]);
+const SIDE_WIRE_POSITIONS = new Float32Array([
+  // top right corner
+  1, 1,
+  // top left corner
+  0, 1,
+  // bottom left corner
+  0, 0,
+  // bottom right corner
+  1, 0
+]);
+
+// Model types
+const TOP_MODEL = 1;
+const SIDE_MODEL = 2;
+
+export default class SolidPolygonLayer extends Layer {
+  getShaders() {
+    return enable64bitSupport(this.props) ?
+      {vs: vs64, fs, modules: ['project64', 'lighting', 'picking']} :
+      {vs, fs, modules: ['lighting', 'picking']}; // 'project' module added by default.
+  }
+
+  initializeState() {
+    const {gl} = this.context;
+    this.setState({
+      models: this._getModels(gl),
+      numInstances: 0,
+      IndexType: gl.getExtension('OES_element_index_uint') ? Uint32Array : Uint16Array
+    });
+
+    const {attributeManager} = this.state;
+    const noAlloc = true;
+    /* eslint-disable max-len */
+    attributeManager.add({
+      indices: {size: 1, modelType: TOP_MODEL, isIndexed: true, update: this.calculateIndices, noAlloc},
+      positions: {size: 3, accessor: ['extruded', 'fp64'], update: this.calculatePositions, noAlloc},
+      nextPositions: {size: 3, modelType: SIDE_MODEL, accessor: ['extruded', 'fp64'], update: this.calculateNextPositions, noAlloc},
+      elevations: {size: 1, accessor: ['extruded', 'getElevation'], update: this.calculateElevations, noAlloc},
+      colors: {size: 4, type: GL.UNSIGNED_BYTE, accessor: 'getColor', update: this.calculateColors, noAlloc},
+      pickingColors: {size: 3, type: GL.UNSIGNED_BYTE, update: this.calculatePickingColors, noAlloc}
+    });
+    /* eslint-enable max-len */
+  }
+
+  updateAttribute({props, oldProps, changeFlags}) {
+    if (props.fp64 !== oldProps.fp64) {
+      const {attributeManager} = this.state;
+      attributeManager.invalidateAll();
+
+      if (props.fp64 && props.coordinateSystem === COORDINATE_SYSTEM.LNGLAT) {
+        /* eslint-disable max-len */
+        attributeManager.add({
+          positions64xyLow: {size: 2, accessor: 'fp64', update: this.calculatePositionsLow},
+          nextPositions64xyLow: {size: 2, modelType: SIDE_MODEL, accessor: 'fp64', update: this.calculateNextPositionsLow}
+        });
+        /* eslint-enable max-len */
+      } else {
+        attributeManager.remove([
+          'positions64xyLow',
+          'nextPositions64xyLow'
+        ]);
+      }
+    }
+  }
+
+  draw({uniforms}) {
+    const {extruded, lightSettings, elevationScale} = this.props;
+    const {viewport} = this.context;
+
+    const renderUniforms = Object.assign({}, uniforms, {
+      extruded: extruded ? 1.0 : 0.0,
+      elevationScale,
+      pixelsPerUnit: viewport.getDistanceScales().pixelsPerDegree
+    },
+    lightSettings);
+
+    this.state.models.forEach(model => {
+      model.render(renderUniforms);
+    });
+  }
+
+  updateState(updateParams) {
+    super.updateState(updateParams);
+
+    this.updateGeometry(updateParams);
+    const {props, oldProps} = updateParams;
+
+    const regenerateModels = props.fp64 !== oldProps.fp64 ||
+      props.extruded !== oldProps.extruded ||
+      props.wireframe !== oldProps.wireframe;
+
+    if (regenerateModels) {
+      this.setState({
+        // Set a flag to set attributes to new models
+        modelsChanged: true,
+        models: this._getModels(this.context.gl)
+      });
+    }
+
+    if (props.extruded !== oldProps.extruded) {
+      this.state.attributeManager.invalidate('extruded');
+    }
+    if (props.fp64 !== oldProps.fp64) {
+      this.state.attributeManager.invalidate('fp64');
+    }
+  }
+
+  updateGeometry({props, oldProps, changeFlags}) {
+    const geometryConfigChanged = changeFlags.dataChanged ||
+      (changeFlags.updateTriggersChanged && (
+        changeFlags.updateTriggersChanged.all ||
+        changeFlags.updateTriggersChanged.getPolygon));
+
+    // When the geometry config  or the data is changed,
+    // tessellator needs to be invoked
+    if (geometryConfigChanged) {
+      // TODO - avoid creating a temporary array here: let the tesselator iterate
+      const polygons = props.data.map(props.getPolygon);
+
+      this.setState({
+        polygonTesselator: new PolygonTesselator(polygons)
+      });
+
+      this.state.attributeManager.invalidateAll();
+    }
+
+    if (geometryConfigChanged ||
+      props.extruded !== oldProps.extruded ||
+      props.fp64 !== oldProps.fp64) {
+      this.state.polygonTesselator.updatePositions({
+        fp64: props.fp64,
+        extruded: props.extruded
+      });
+    }
+  }
+
+  updateAttributes(props) {
+    const {attributeManager, modelsChanged} = this.state;
+
+    // Figure out data length
+    attributeManager.update({
+      data: props.data,
+      numInstances: 0,
+      props,
+      buffers: props,
+      context: this,
+      // Don't worry about non-attribute props
+      ignoreUnknownAttributes: true
+    });
+
+    if (modelsChanged) {
+      this._updateAttributes(attributeManager.attributes);
+      // clear the flag
+      this.setState({modelsChanged: false});
+    } else {
+      const changedAttributes = attributeManager.getChangedAttributes({clearChangedFlags: true});
+      this._updateAttributes(changedAttributes);
+    }
+  }
+
+  _updateAttributes(attributes) {
+    this.state.models.forEach(model => {
+      const isTop = !model.uniforms.isSideVertex;
+
+      if (isTop) {
+        model.setVertexCount(this.state.numVertex);
+      } else {
+        model.setInstanceCount(this.state.numInstances);
+      }
+
+      const newAttributes = {};
+      for (const attributeName in attributes) {
+        const attribute = attributes[attributeName];
+
+        if ((isTop && attribute.modelType !== SIDE_MODEL) ||
+          (!isTop && attribute.modelType !== TOP_MODEL)) {
+          newAttributes[attributeName] = {
+            isIndexed: attribute.isIndexed,
+            instanced: !isTop || (!this.props.extruded && attributeName === 'elevations'),
+            size: attribute.size,
+            value: attribute.value
+          };
+        }
+      }
+      model.setAttributes(newAttributes);
+    });
+  }
+
+  _getModels(gl) {
+    const {id, extruded, wireframe} = this.props;
+
+    return [
+      !wireframe && new Model(gl, Object.assign({}, this.getShaders(), {
+        id: `${id}-top`,
+        geometry: new Geometry({
+          drawMode: GL.TRIANGLES,
+          attributes: {
+            vertexPositions: {size: 2, instanced: 1, value: new Float32Array([0, 1])}
+          }
+        }),
+        uniforms: {
+          isSideVertex: 0
+        },
+        instanceCount: 1,
+        vertexCount: 0,
+        isInstanced: true,
+        isIndexed: true,
+        shaderCache: this.context.shaderCache
+      })),
+
+      extruded && new Model(gl, Object.assign({}, this.getShaders(), {
+        id: `${id}-side`,
+        geometry: new Geometry({
+          drawMode: wireframe ? GL.LINE_STRIP : GL.TRIANGLE_STRIP,
+          vertexCount: 4,
+          attributes: {
+            vertexPositions: {size: 2, value: wireframe ? SIDE_WIRE_POSITIONS : SIDE_FILL_POSITIONS}
+          }
+        }),
+        uniforms: {
+          isSideVertex: 1
+        },
+        isInstanced: true,
+        shaderCache: this.context.shaderCache
+      }))
+    ].filter(Boolean);
+
+  }
+
+  calculateIndices(attribute) {
+    attribute.value = this.state.polygonTesselator.indices();
+    attribute.target = GL.ELEMENT_ARRAY_BUFFER;
+    const numVertex = attribute.value.length / attribute.size;
+    this.setState({numVertex});
+  }
+
+  calculatePositions(attribute) {
+    attribute.value = this.state.polygonTesselator.positions();
+    const numInstances = attribute.value.length / attribute.size;
+    this.setState({numInstances});
+  }
+  calculatePositionsLow(attribute) {
+    attribute.value = this.state.polygonTesselator.positions64xyLow();
+  }
+
+  calculateNextPositions(attribute) {
+    attribute.value = this.state.polygonTesselator.nextPositions();
+  }
+  calculateNextPositionsLow(attribute) {
+    attribute.value = this.state.polygonTesselator.nextPositions64xyLow();
+  }
+
+  calculateElevations(attribute) {
+    if (this.props.extruded) {
+      attribute.value = this.state.polygonTesselator.elevations({
+        getElevation: polygonIndex => this.props.getElevation(this.props.data[polygonIndex])
+      });
+    } else {
+      attribute.value = new Float32Array(1);
+    }
+  }
+
+  calculateColors(attribute) {
+    attribute.value = this.state.polygonTesselator.colors({
+      getColor: polygonIndex => this.props.getColor(this.props.data[polygonIndex])
+    });
+  }
+
+  // Override the default picking colors calculation
+  calculatePickingColors(attribute) {
+    attribute.value = this.state.polygonTesselator.pickingColors();
+  }
+}
+
+SolidPolygonLayer.layerName = 'SolidPolygonLayer';
+SolidPolygonLayer.defaultProps = defaultProps;
